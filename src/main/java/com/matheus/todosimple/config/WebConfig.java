032e555ec28e2e4ff6a64cd30e3af37dd1c9063c
Matheus Bergamo <matheusbergamo@hotmail.com>
2023-05-04 16:46:25 -0300

    config

--- src/main/java/com/matheus/todosimple/config/WebConfig.java
+++ src/main/java/com/matheus/todosimple/config/WebConfig.java
@@ -1,2 +1,16 @@
-package com.matheus.todosimple.config;public class WebConfig {
+package com.matheus.todosimple.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {//implementar o WebSvc para identificar para o Spring que será uma config Spring
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
 }
